1b90c1d82cb604d49232725cdd618319d683761c
Ryutaro <ryutaro@RMBA.local>
2023-02-13 15:55:30 +0900

    fix plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,6 +1,18 @@
-vim.cmd.packadd "packer.nvim"
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+      fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+      vim.cmd [[packadd packer.nvim]]
+      return true
+    end
+    return false
+end
 
-return require('packer').startup(function()
+local packer_bootstrap = ensure_packer()
+-- vim.cmd.packadd "packer.nvim"
+
+return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     -- Theme
@@ -88,4 +100,8 @@ return require('packer').startup(function()
 	use { 'fatih/vim-go', opt = true, ft = { 'go' } }
 
 	use 'phaazon/hop.nvim'
+
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
