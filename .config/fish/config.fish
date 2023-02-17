0d03807cb7b6e3d68ef231e6d6d34e2825b5c53e
Ryutaro <ryutaro@RMBA.local>
2023-02-18 02:23:58 +0900

    add brew

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,10 @@ if status is-interactive
 end
 set PATH /opt/homebrew/bin $PATH
 
+# Go
+set -x GOPATH $HOME/go
+set -x PATH $PATH $GOPATH/bin
+
 abbr -a vim nvim
 abbr -a ll exa -la --icons
 abbr -a cat bat -p
