eafe055170f8f6bcf67a3cbafb85754502a93175
Ryutaro <ryutaro9595@gmail.com>
2024-05-01 01:00:17 +0900

    resize neotree window

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -136,27 +136,9 @@ return {
             open = function()
               vim.api.nvim_input("<esc><space>e")
             end,
-            size = { height = 0.5, width = 0.15 },
+            size = { height = 1.0, width = 0.3 },
           },
           { title = "Neotest Summary", ft = "neotest-summary" },
-          -- {
-          --   title = "Neo-Tree Git",
-          --   ft = "neo-tree",
-          --   filter = function(buf)
-          --     return vim.b[buf].neo_tree_source == "git_status"
-          --   end,
-          --   pinned = true,
-          --   open = "Neotree position=right git_status",
-          -- },
-          -- {
-          --   title = "Neo-Tree Buffers",
-          --   ft = "neo-tree",
-          --   filter = function(buf)
-          --     return vim.b[buf].neo_tree_source == "buffers"
-          --   end,
-          --   pinned = true,
-          --   open = "Neotree position=top buffers",
-          -- },
           "neo-tree",
         },
         keys = {
