fb56b8a27f350f3de98fb2a5a3cdd5010f76f0cc
Ryutaro <ryutaro9595@gmail.com>
2024-05-19 02:02:59 +0900

    fixed wezterm

--- .config/wezterm/.wezterm.lua
+++ .config/wezterm/.wezterm.lua
@@ -5,41 +5,105 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+local act = wezterm.action
+
+-- wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width) end)
+
 config = {
-	color_scheme = "Tokyo Night Storm",
+	window_background_opacity = 0.85,
+	macos_window_background_blur = 60,
+
+	-- color_scheme = "Tokyo Night Storm",
+	color_scheme = "Ayu Mirage",
+	colors = {
+		selection_fg = "none",
+		selection_bg = "rgba(50% 50% 50% 50%)",
+
+		-- Tab style
+		tab_bar = {
+			active_tab = {
+				bg_color = "#E95678",
+				fg_color = "#1E1E32",
+			},
+			inactive_tab = {
+				bg_color = "#2E303E",
+				fg_color = "#646482",
+			},
+		},
+	},
+
+	-- Tab
+	tab_bar_at_bottom = true,
+	hide_tab_bar_if_only_one_tab = true,
+	window_frame = {
+		font_size = 13.0,
+		inactive_titlebar_bg = "none",
+		active_titlebar_bg = "none",
+	},
+
 	-- Font settings
 	font = wezterm.font("Hack Nerd Font", {
-		weight = "Bold",
 		stretch = "Normal",
 		style = "Normal",
 	}),
-	font_size = 14,
+
+	window_padding = {
+		left = 30,
+		right = 30,
+		top = 60,
+		bottom = 0,
+	},
+
+	-- general options
+	adjust_window_size_when_changing_font_size = false,
+	-- enable_tab_bar = false,
+	debug_key_events = false,
+	native_macos_fullscreen_mode = false,
+
+	-- Window
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE",
+
+	-- Text
+	font_size = 13,
 	cell_width = 1.1,
 	line_height = 1.3,
-	-- Title bar
-	window_decorations = "INTEGRATED_BUTTONS | RESIZE",
-	-- Tab bar
-	tab_max_width = 20,
-	use_fancy_tab_bar = false,
 
-	initial_rows = 30,
-	initial_cols = 100,
+	initial_rows = 60,
+	initial_cols = 150,
 	use_ime = true,
 
 	leader = { key = "t", mods = "CTRL", timeout_milliseconds = 1000 },
-	keys = {
-		-- Split Pane
-		{ key = "v", mods = "LEADER", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-		{ key = "s", mods = "LEADER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
-		{ key = "x", mods = "LEADER", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
-		{ key = "z", mods = "LEADER", action = wezterm.action.TogglePaneZoomState },
+	-- key maps
 
-		-- Switch Pane
-		{ key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") },
-		{ key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Right") },
-		{ key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up") },
-		{ key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down") },
+	keys = {
+		{ key = "t", mods = "CTRL", action = act.PaneSelect({ alphabet = "kjhlnm" }) },
+		-- Split Pane
+		{
+			key = "S",
+			mods = "CTRL",
+			action = wezterm.action.SplitPane({ direction = "Down", size = { Percent = 30 } }),
+		},
 	},
+	key_tables = {
+		copy_mode = {
+			{
+				key = "y",
+				mods = "NONE",
+				action = act.Multiple({
+					act.CopyTo("PrimarySelection"),
+					wezterm.action.ClearSelection,
+					-- clear the selection mode, but remain in copy mode
+					wezterm.action.CopyMode({ "ClearSelectionMode" }),
+				}),
+			},
+		},
+	},
+	-- key_tables = {
+	-- 	copy_mode = {
+	-- 		{ key = "C", mods = "CTRL", action = wezterm.action.CopyMode({ SetSelectionMode = "Block" }) },
+	-- 	},
+	-- },
 }
 
 return config
---
+++ .config/wezterm/README.md
@@ -0,0 +1,8 @@
+## Keymaps
+
+| Name               | Keys             | Description                                                    |
+| ------------------ | ---------------- | -------------------------------------------------------------- |
+| Copy mode          | Ctrl + Shift + x | vimライクなコピーモードを起動                                  |
+| Select panel mode  | Ctrl + t         | パネル選択モードを起動して、移動したいパネルに紐づくキーを選択 |
+| Split panel bottom | Ctrl + Shift + s | 下方向に高さ30%パネルを生成                                    |
+| Zoom current panel | Ctrl + Shift + z | アクティブなパネルを最大化する                                 |
