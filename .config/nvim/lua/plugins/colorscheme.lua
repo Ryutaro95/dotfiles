7f6884afb811defca71a6f1fac589af4a7e8dfad
Ryutaro <ryutaro9595@gmail.com>
2025-09-09 22:09:34 +0900

    clean up colorscheme config

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,69 +1,5 @@
 -- local macchiato = require("catppuccin.palettes").get_palette("macchiato")
 return {
-  -- {
-  --   "AlexvZyl/nordic.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require("nordic").setup({
-  --       -- transparent = {
-  --       --   bg = true,
-  --       -- },
-  --     })
-  --     require("nordic").load()
-  --   end,
-  -- },
-
-  -- {
-  --   "Shatur/neovim-ayu",
-  --   config = function()
-  --     require("ayu").setup({
-  --       mirage = true,
-  --       overrides = {
-  --         Normal = { bg = "None" },
-  --         ColorColumn = { bg = "None" },
-  --         SignColumn = { bg = "None" },
-  --         Folded = { bg = "None" },
-  --         FoldColumn = { bg = "None" },
-  --         -- CursorLine = { bg = "None" },
-  --         CursorColumn = { bg = "None" },
-  --         WhichKeyFloat = { bg = "None" },
-  --         VertSplit = { bg = "None" },
-  --         LspInlayHint = { fg = "#a0a0a0", bg = "None", italic = true },
-  --         Comment = { fg = "#6272A4", bg = "None", italic = true },
-  --         LineNr = { fg = "#808080", bg = "None" }, -- 行番号
-  --         -- NeoTreeNormal = { fg = "#DED9D9", bg = "#1A1F29" },
-  --         -- NeoTreeNormalNC = { fg = "#DED9D9", bg = "#1A1F29" },
-  --       },
-  --     })
-  --   end,
-  -- },
-
-  -- {
-  --   "projekt0n/github-nvim-theme",
-  --   name = "github-theme",
-  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  --   priority = 1000, -- make sure to load this before all the other start plugins
-  --   config = function()
-  --     require("github-theme").setup({
-  --       options = {
-  --
-  --         transparent = true,
-  --       },
-  --     })
-  --
-  --     -- vim.cmd("colorscheme github_dark")
-  --   end,
-  -- },
-
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     -- colorscheme = "ayu",
-  --     colorscheme = "github_dark",
-  --   },
-  -- },
-
   {
     "navarasu/onedark.nvim",
     opts = {
@@ -75,8 +11,6 @@ return {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "onedark",
-      -- colorscheme = "ayu",
-      -- colorscheme = "github_dark",
     },
   },
 }
