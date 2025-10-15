60f1fe9e429a41f0006a804388afe97a1260a0ba
Ryutaro <ryutaro9595@gmail.com>
2025-10-16 02:15:20 +0900

    fix tab width

--- archlinux/.config/nvim/lua/config/autocmds.lua
+++ archlinux/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Go filetype settings
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  callback = function()
+    vim.opt_local.tabstop = 4 -- タブ文字の表示幅を4に設定
+    vim.opt_local.shiftwidth = 4 -- インデント幅を4に設定
+    vim.opt_local.softtabstop = 4 -- タブキー押下時の幅を4に設定
+  end,
+})
