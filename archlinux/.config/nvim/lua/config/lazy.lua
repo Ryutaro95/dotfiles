9199bbbd53b45dd357dbd67400fcde674831a169
Ryutaro <ryutaro9595@gmail.com>
2025-10-21 18:17:41 +0900

    add rust

--- archlinux/.config/nvim/lua/config/lazy.lua
+++ archlinux/.config/nvim/lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
     { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
