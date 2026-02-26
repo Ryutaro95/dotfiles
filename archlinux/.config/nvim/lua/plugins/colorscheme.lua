63adb758c7100b3869a6acf86ce182c4dcea4bff
Ryutaro <ryutaro9595@gmail.com>
2026-02-26 21:30:17 +0900

    update

--- archlinux/.config/nvim/lua/plugins/colorscheme.lua
+++ archlinux/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,9 @@ return {
     "nickkadutskyi/jb.nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
+    opts = {
+      transparent = false, -- 背景色を有効にする
+    },
     config = function()
       vim.cmd("colorscheme jb")
     end,
