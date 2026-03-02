b5804c6f708d253c748942ebae0354f80425e43a
Ryutaro <ryutaro9595@gmail.com>
2026-03-02 17:32:05 +0900

    update

--- archlinux/.config/nvim/lua/plugins/colorscheme.lua
+++ archlinux/.config/nvim/lua/plugins/colorscheme.lua
@@ -4,10 +4,14 @@ return {
     lazy = false,
     priority = 1000,
     opts = {
-      transparent = false, -- 背景色を有効にする
+      transparent = true,
     },
     config = function()
       vim.cmd("colorscheme jb")
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+      local comment_hl = vim.api.nvim_get_hl(0, { name = "Comment", link = false })
+      vim.api.nvim_set_hl(0, "LspInlayHint", { bg = "none", fg = comment_hl.fg, bold = true })
     end,
   },
 }
--- archlinux/.config/nvim/lua/plugins/markview.lua
+++ archlinux/.config/nvim/lua/plugins/markview.lua
@@ -2,6 +2,9 @@
 return {
   "OXY2DEV/markview.nvim",
   lazy = false,
+  opts = {
+    initial_state = false,
+  },
 
   -- For blink.cmp's completion
   -- source
