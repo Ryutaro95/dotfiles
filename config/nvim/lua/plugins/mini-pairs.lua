953f411a9eadb4899f37f02ed4aab8d78a5552cb
Ryutaro <ryutaro9595@gmail.com>
2026-04-09 23:32:46 +0900

    生成されたペアを削除されるように設定

--- config/nvim/lua/plugins/mini-pairs.lua
+++ config/nvim/lua/plugins/mini-pairs.lua
@@ -1,5 +1,10 @@
 vim.pack.add({
     "https://github.com/nvim-mini/mini.pairs",
 })
-
 require("mini.pairs").setup({})
+
+-- Delete the generated pair as well.
+vim.keymap.set("i", "<C-h>", "v:lua.MiniPairs.bs()", {
+    expr = true,
+    replace_keycodes = false,
+})
