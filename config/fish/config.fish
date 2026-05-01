596fbc8aed45908359e3f0b5f951b34269c43666
Ryutaro <ryutaro9595@gmail.com>
2026-05-01 12:04:56 +0900

    update

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,103 +1,24 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    set -g fish_greeting ""
-end
 set PATH /opt/homebrew/bin $PATH
 set -x LANG ja_JP.UTF-8
 # Neovim
 set -gx PATH $PATH /opt/nvim-linux-x86_64/bin
-
 # Go
 set -x GOPATH $HOME/go
 set -x PATH $PATH $GOPATH/bin
-
 # Rust
 set -U fish_user_paths $fish_user_paths $HOME/.cargo/bin
-
-# Deno
-set -U fish_user_paths /opt/homebrew/opt/llvm/bin $fish_user_paths
-
 # Claude Code
 set -gx PATH ~/.npm-global/bin $PATH
 
-bind \ct ''
-bind -M insert \ct ''
-bind -M default \ct ''
+abbr -a ll eza -alF --icons
 
-# Editor aliases
-if command -v nvim >/dev/null 2>&1
-    abbr -a vim nvim
-    abbr -a v nvim
-end
-
-# File listing aliases
-if command -v eza >/dev/null 2>&1
-    abbr -a ll eza -alF --icons
-    abbr -a tree eza -alF --icons -T
-else
-    abbr -a ll ls -alF
-    abbr -a tree tree
-end
-
-# Cat alternative
-if command -v bat >/dev/null 2>&1
-    abbr -a cat bat -p
-end
-
-# Docker aliases
+abbr -a cat bat -p
 abbr -a dc docker compose
 abbr -a cd z
-
-if command -v lazygit >/dev/null 2>&1
-    abbr -a lg lazygit
-end
-
-if command -v lazydocker >/dev/null 2>&1
-    abbr -a ld lazydocker
-end
-# Terminal multiplexer
-abbr -a tm tmux
-
-# rbenv
-if command -v rbenv >/dev/null 2>&1
-    status --is-interactive; and rbenv init - fish | source
-end
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/ryutaro/Downloads/google-cloud-sdk/path.fish.inc' ]
-    . '/Users/ryutaro/Downloads/google-cloud-sdk/path.fish.inc'
-end
+abbr -a lg lazygit
+abbr -a ld lazydocker
 
 # Starship - https://starship.rs/ja-JP/guide/
-if command -v starship >/dev/null 2>&1
-    starship init fish | source
-end
-
+starship init fish | source
 # zoxide
-if command -v zoxide >/dev/null 2>&1
-    zoxide init fish | source
-end
-
-# NVM using bass to run bash nvm in fish
-function nvm
-    bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
-end
-
-set -x NVM_DIR ~/.nvm
-function __check_nvm --on-variable PWD --description 'Do nvm stuff'
-    status --is-command-substitution; and return
-
-    if test -f .nvmrc; and test -r .nvmrc
-        nvm use
-    else
-    end
-end
-
-# tmux自動起動
-if test -z "$TMUX" && status --is-login && test "$TERM" = xterm-ghostty
-    tmux attach-session -t main; or tmux new-session -s main
-end
-
-# Added by Antigravity
-fish_add_path /Users/ryutaro/.antigravity/antigravity/bin
-abbr -a vv 'NVIM_APPNAME=nvim-new nvim'
+zoxide init fish | source
